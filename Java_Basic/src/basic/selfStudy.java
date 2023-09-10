06697d0a7f4c218ed2eaa7dff815c726c28bdb16
yoojin <yoojin9199@gmail.com>
2023-09-10 22:57:36 +0900

    basic cd practices at home

--- Java_Basic/src/basic/selfStudy.java
+++ Java_Basic/src/basic/selfStudy.java
@@ -9,48 +9,37 @@ public class selfStudy {
 	
     public static void main(String[] args) {
 		
-    	FileWriter fw = null;
-
-		try {
-			fw = new FileWriter("C:/User/yooji/Desktop/test1.txt");
-
-			// \r: 캐리지 리턴 -> 커서를 맨 앞으로 땡기는 동작.
-            // \n: 줄 개행(줄바꿈)
-            // 상황과 환경에 따라서 줄 개행 시 커서를 맨 앞으로 안땡겨주는 경우가 있습니다.
-			String str = "첫번째 문장입나더,"; //\r: 캐리지리턴. 커서를 맨 앞으로 땡기고 줄개행
-
-			fw.write(str);
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				fw.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
+    	/*
+    	// n과 m의 공배수를 구하는 문제
+    	public int solution(int number, int n, int m) {
+            return number % n == 0 && number % m == 0 ? 1 : 0;
+        }
+        
+        //return number % (n* m) == 0 ? 1 : 0;의 경우, 10과 20의 공배수를 구할 수 없어서 예외가 생김(문제 제한이 number<=100 이었음)
+         * 
+         * 
+        */
+   
     	
-//    	FileWriter fw = null;
-//
-//    	try{
-//    			
-//    			fw = new FileWriter("C:/User/yooji/Desktop/test1.txt");
-//
-//    			String str = "첫번째 문장입니다.";
-//    			
-//    			fw.write(str);
-//
-//    	} catch (Exception e) { // 예외처리부분 대충 썼으니 자세히는 상단의 FileOutputStream 참조
-//    			e.printStackTrace();
-//    	} finally {
-//    			try {
-//					fw.close();
-//				} catch (IOException e) {
-//					e.printStackTrace();
-//				}
-//    	}
-//    	System.out.println("프로그램이 정상 종료");
+    	/*
+    	// n 이 홀수: n 이하 모든 홀수의 합, 짝수: n 이하 모든 짝수의 제곱의 합.
+    	boolean isOdd = n%2 != 0 ? true : false ;
+        int answer = 0;
+        
+        if(isOdd){
+            for(int i=1;i<=n;i+=2) {
+                answer += i;
+            }
+        } else {
+            for(int i=2;i<=n;i+=2) {
+                answer += i*i;
+            }
+        } 
+        return answer;
+    	 */
+        
+    	
+    	
 	}
 }	
 
